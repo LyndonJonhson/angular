44f1713fbd24f5931bc09de165f999b50283b569
lyndon.jonhson.700 <lyndon.jonhson.700@ufrn.edu.br>
2024-01-04 12:02:32 -0300

    serviços

---
+++ servicos/src/app/cursos/cursos.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css'],
+  providers: [CursosService]
+})
+export class CursosComponent {
+
+  cursos: string[] = [];
+
+  //cursosService: CursosService;
+
+  constructor(private cursosService: CursosService) {
+    //this.cursosService = new CursosService;
+    // this.cursosService = _cursosService;
+  }
+
+  ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
+
+    CursosService.criouNovoCurso.subscribe(
+      curso => this.cursos.push(curso)
+    );
+  }
+
+}
